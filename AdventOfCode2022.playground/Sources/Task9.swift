540a91492f1ae8387f20b0171b1287e566c21a22
Ondra Mařík <ondra.marik@gmail.com>
2022-12-13 19:46:37 +0100

    Solve task 9b

--- AdventOfCode2022.playground/Sources/Task9.swift
+++ AdventOfCode2022.playground/Sources/Task9.swift
@@ -3,40 +3,99 @@ import Foundation
 
 public class Task9 {
     
-    public enum Direction {
+    enum Direction {
         case x
         case y
     }
     
-    public struct Step {
-        public let direction: Direction
-        public let amount: Int
+    struct Coordinate: Hashable, CustomStringConvertible {
+        var x: Int
+        var y: Int
+        
+        mutating func add(_ other: Coordinate) {
+            x += other.x
+            y += other.y
+        }
+        
+        mutating func moveTo(_ other: Coordinate) {
+            let deltaX = other.x - x
+            let deltaY = other.y - y
+            if abs(deltaX) > 1 && abs(deltaY) > 1 {
+                x += deltaX.signum()
+                y += deltaY.signum()
+            } else if abs(deltaX) > 1 {
+                x += deltaX.signum()
+                y = other.y
+            } else if abs(deltaY) > 1 {
+                x = other.x
+                y += deltaY.signum()
+            }
+        }
+        
+        var description: String {
+            return "(\(x),\(y))"
+        }
+        
+    }
+    
+    struct Step {
+        let direction: Direction
+        let amount: Int
+        let delta: Coordinate
         
         init(direction: String, amount: Int) {
+            self.amount = amount
             switch direction {
             case "R":
                 self.direction = .x
-                self.amount = amount
+                self.delta = Coordinate(x: 1, y: 0)
             case "L":
                 self.direction = .x
-                self.amount = -amount
+                self.delta = Coordinate(x: -1, y: 0)
             case "U":
                 self.direction = .y
-                self.amount = amount
+                self.delta = Coordinate(x: 0, y: 1)
             case "D":
                 self.direction = .y
-                self.amount = -amount
+                self.delta = Coordinate(x: 0, y: -1)
             default:
                 fatalError("Uknown direction in input data")
             }
         }
-        
-        public var isPositive: Bool {
-            return amount >= 0
-        }
     }
     
-    public static func loadSteps() -> [Step] {
+    class Rope: CustomStringConvertible {
+        
+        var rope: [Coordinate]
+        var visited: Set<Coordinate>
+        
+        init(ropeLength: Int) {
+            rope = Array(repeating: Coordinate(x: 0, y: 0), count: ropeLength)
+            visited = Set(arrayLiteral: Coordinate(x: 0, y: 0))
+        }
+        
+        func move(step: Step) {
+            for _ in 1...step.amount {
+                for index in rope.indices {
+                    if index == 0 {
+                        rope[index].add(step.delta)
+                    } else {
+                        rope[index].moveTo(rope[index - 1])
+                        if index == rope.count-1 {
+                            visited.insert(rope[index])
+                        }
+                    }
+                }
+            }
+        }
+        
+        var description: String {
+            return "\(rope.map { $0.description }.joined(separator: " ")), visited: \(visited.count)\n\(visited)"
+        }
+        
+    }
+    
+    static func loadSteps() -> [Step] {
         let data = load(file: "Input9a")
         let parsedData = data!.split(separator: "\n")
         
@@ -47,11 +106,25 @@ public class Task9 {
     }
     
     public static func computeA() {
-        
+        let steps = loadSteps()
+        let rope = Rope(ropeLength: 2)
+
+        steps.forEach { step in
+            rope.move(step: step)
+        }
+
+        print("Rope visited \(rope.visited.count) positions at least once")
     }
     
     public static func computeB() {
-        
+        let steps = loadSteps()
+        let rope = Rope(ropeLength: 10)
+
+        steps.forEach { step in
+            rope.move(step: step)
+        }
+
+        print("Rope visited \(rope.visited.count) positions at least once")
     }
     
 }
